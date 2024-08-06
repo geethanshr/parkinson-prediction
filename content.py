faa5316b6a7b292786ae301254a3cac9db30a440
Geethansh Rathore <75798750+geethanshr@users.noreply.github.com>
2024-08-06 21:27:21 +0530

    Update content.py

--- content.py
+++ content.py
@@ -11,7 +11,7 @@ def by_the_team():
                 <img src="https://media.licdn.com/dms/image/D5635AQFsyf2F1HdMJA/profile-framedphoto-shrink_400_400/0/1707495992128?e=1723564800&v=beta&t=tokb0f-cZJU3JtLjwWD4QcS-UC9AV2hVnd-0vpKGCHw" style="width: 100px; height: 100px; vertical-align: middle;">
                 Geethansh's LinkedIn
             </a>
-            <p><strong>Project Link:</strong> <a href="#">Project</a></p>
+            <p><strong>Post Link:</strong> <a href="#">Project</a></p>
             <p><strong>Challenges Tackled:</strong></p>
             <ul>
                 <li><strong>Problem 1:</strong> Description of how the problem was solved.</li>
@@ -29,7 +29,7 @@ def by_the_team():
                 <img src="https://media.licdn.com/dms/image/D5603AQFDLRLPwQ3n5A/profile-displayphoto-shrink_400_400/0/1718229540129?e=1728518400&v=beta&t=VFpe8wrta4oPO6ExWyOUVRpLS_IZ7hVvgXhOqdQoz_U" style="width: 100px; height: 100px; vertical-align: middle;">
                 Gourav's LinkedIn
             </a>
-            <p><strong>Project Link:</strong> <a href="#">Project</a></p>
+            <p><strong>Post Link:</strong> <a href="#">Project</a></p>
             <p><strong>Challenges Tackled:</strong></p>
             <ul>
                 <li><strong>Problem 1:</strong> Description of how the problem was solved.</li>
