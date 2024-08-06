6f8e136ad69c6aaf451a6868a574290179bac343
Geethansh Rathore <75798750+geethanshr@users.noreply.github.com>
2024-08-06 19:58:26 +0530

    Update content.py

--- content.py
+++ content.py
@@ -1,7 +1,34 @@
-import numpy as np
 import streamlit as st
 
 def by_the_team():
     st.title("By the Team")
 
-  
+    st.write("### Shivam's Contributions")
+    st.markdown("""
+    <blockquote>
+        [![Shivam's LinkedIn](https://img.shields.io/badge/Shivam-LinkedIn-blue)](https://www.linkedin.com/in/shivam/)
+        <br><br>
+        **Project Link:** [Project](#)
+        <br><br>
+        **Challenges Tackled:**
+        <br><br>
+        - **Problem 1:** Description of how the problem was solved.
+        <br><br>
+        - **Problem 2:** Description of how the problem was solved.
+    </blockquote>
+    """, unsafe_allow_html=True)
+
+    st.write("### Gourav's Contributions")
+    st.markdown("""
+    <blockquote>
+        [![Gourav's LinkedIn](https://img.shields.io/badge/Gourav-LinkedIn-blue)](https://www.linkedin.com/in/gourav/)
+        <br><br>
+        **Project Link:** [Project](#)
+        <br><br>
+        **Challenges Tackled:**
+        <br><br>
+        - **Problem 1:** Description of how the problem was solved.
+        <br><br>
+        - **Problem 2:** Description of how the problem was solved.
+    </blockquote>
+    """, unsafe_allow_html=True)
