2b465614def871a601e10902e41bc3e490c66a10
Geethansh Rathore <75798750+geethanshr@users.noreply.github.com>
2024-08-06 21:40:50 +0530

    Update content.py

--- content.py
+++ content.py
@@ -3,15 +3,15 @@ import streamlit as st
 def by_the_team():
     #st.title("By the Team")
 
-    #st.write("### Geethansh's Contributions")
+    #st.write("### Gourav")
     st.markdown(
         """
         <div style="border: 1px solid #e6e6e6; padding: 20px; border-radius: 10px;">
-            <a href="https://www.linkedin.com/in/geethanshr/" target="_blank">
-                <img src="https://media.licdn.com/dms/image/D5635AQFsyf2F1HdMJA/profile-framedphoto-shrink_400_400/0/1707495992128?e=1723564800&v=beta&t=tokb0f-cZJU3JtLjwWD4QcS-UC9AV2hVnd-0vpKGCHw" style="width: 100px; height: 100px; vertical-align: middle;">
-                Geethansh's LinkedIn
+            <a href="https://www.linkedin.com/in/gourav-pandey-a4505a29a/" target="_blank">
+                <img src="https://media.licdn.com/dms/image/D5603AQFDLRLPwQ3n5A/profile-displayphoto-shrink_400_400/0/1718229540129?e=1728518400&v=beta&t=VFpe8wrta4oPO6ExWyOUVRpLS_IZ7hVvgXhOqdQoz_U" style="width: 100px; height: 100px; vertical-align: middle;">
+                Gourav's LinkedIn
             </a>
-            <p><strong>Post Link:</strong> <a href="https://www.linkedin.com/posts/geethanshr_powerbi-nocode-businessintelligence-activity-7210502366268207104-Cjix?utm_source=share&utm_medium=member_desktop">Click here</a></p>
+            <p><strong>About the Model:</strong> <a href="#">Click here</a></p>
             <blockquote>
                     <p><strong>Challenges Tackled:</strong></p>
                     <p>"One of the major challenges we faced was integrating multiple data sources. We overcame this by using advanced ETL processes."</p>
@@ -22,15 +22,16 @@ def by_the_team():
         """, 
         unsafe_allow_html=True
     )
-    #st.write("### Gourav")
+
+    #st.write("### Geethansh's Contributions")
     st.markdown(
         """
         <div style="border: 1px solid #e6e6e6; padding: 20px; border-radius: 10px;">
-            <a href="https://www.linkedin.com/in/gourav-pandey-a4505a29a/" target="_blank">
-                <img src="https://media.licdn.com/dms/image/D5603AQFDLRLPwQ3n5A/profile-displayphoto-shrink_400_400/0/1718229540129?e=1728518400&v=beta&t=VFpe8wrta4oPO6ExWyOUVRpLS_IZ7hVvgXhOqdQoz_U" style="width: 100px; height: 100px; vertical-align: middle;">
-                Gourav's LinkedIn
+            <a href="https://www.linkedin.com/in/geethanshr/" target="_blank">
+                <img src="https://media.licdn.com/dms/image/D5635AQFsyf2F1HdMJA/profile-framedphoto-shrink_400_400/0/1707495992128?e=1723564800&v=beta&t=tokb0f-cZJU3JtLjwWD4QcS-UC9AV2hVnd-0vpKGCHw" style="width: 100px; height: 100px; vertical-align: middle;">
+                Geethansh's LinkedIn
             </a>
-            <p><strong>Post Link:</strong> <a href="#">Project</a></p>
+            <p><strong>About the deployment:</strong> <a href="https://www.linkedin.com/posts/geethanshr_powerbi-nocode-businessintelligence-activity-7210502366268207104-Cjix?utm_source=share&utm_medium=member_desktop">Click here</a></p>
             <blockquote>
                     <p><strong>Challenges Tackled:</strong></p>
                     <p>"One of the major challenges we faced was integrating multiple data sources. We overcame this by using advanced ETL processes."</p>
