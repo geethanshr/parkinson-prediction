cbcb2115833ec3359ab015c5223e8902a4edab1c
Geethansh Rathore <75798750+geethanshr@users.noreply.github.com>
2024-08-06 21:22:33 +0530

    Update content.py

--- content.py
+++ content.py
@@ -8,7 +8,7 @@ def by_the_team():
         """
         <div style="border: 1px solid #e6e6e6; padding: 20px; border-radius: 10px;">
             <a href="https://www.linkedin.com/in/geethanshr/" target="_blank">
-                <img src="https://media.licdn.com/dms/image/D5635AQFsyf2F1HdMJA/profile-framedphoto-shrink_400_400/0/1707495992128?e=1723564800&v=beta&t=tokb0f-cZJU3JtLjwWD4QcS-UC9AV2hVnd-0vpKGCHw" style="width: 24px; height: 24px; vertical-align: middle;">
+                <img src="https://media.licdn.com/dms/image/D5635AQFsyf2F1HdMJA/profile-framedphoto-shrink_400_400/0/1707495992128?e=1723564800&v=beta&t=tokb0f-cZJU3JtLjwWD4QcS-UC9AV2hVnd-0vpKGCHw" style="width: 100px; height: 100px; vertical-align: middle;">
                 Geethansh's LinkedIn
             </a>
             <p><strong>Project Link:</strong> <a href="#">Project</a></p>
