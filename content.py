ed56c3b8a5ffedca46b0ca9a4321976c85d943e6
Geethansh Rathore <75798750+geethanshr@users.noreply.github.com>
2024-08-06 19:53:25 +0530

    Update content.py

--- content.py
+++ content.py
@@ -1,8 +1,8 @@
 import numpy as np
 import streamlit as st
 
-def by_the_creators():
-    st.title("By the Creators")
+def by_the_team():
+    st.title("By the Team")
 
     st.write("### Shivam's Contributions")
     st.markdown("""
